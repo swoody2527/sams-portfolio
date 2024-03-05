173688f37ccc9e1e409a28ac30f88dfc8a0f57b7
swoody2527 <swoodall2527@gmail.com>
2024-03-05 20:34:55 +0000

    added about-me

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,87 @@ import { useInView } from "react-intersection-observer";
 
 function App() {
   const [aboutMeRef, aboutMeInView] = useInView({
-    triggerOnce: true
-  })
+    triggerOnce: true,
+  });
+
+  const [skillsetRef, skillsetInView] = useInView({
+    triggerOnce: true,
+  });
+
+  const [contactRef, contactInView] = useInView({
+    triggerOnce: true,
+  });
 
   return (
     <div className="app-container">
       <NavBar></NavBar>
       <div className="main-container">
-
         <div className="main-content-hero">
-        <div className="typewriter">
-          <h1>
-            Hello! I'm <span className="my-name">Sam</span>.
-          </h1>
-        </div>
+          <div className="typewriter">
+            <h1>
+              Hello! I'm <span className="my-name">Sam</span>.
+            </h1>
+          </div>
           <div className="hero-elements">
             <p className="hero-text">
-              I am a <span style={{color: "white"}}>Software Engineer</span> who loves forging great programming
-              solutions and exploring new technologies. Welcome to my portfolio
-              website.
+               A <span style={{ color: "white" }}>Software Engineer</span>{" "}
+              who loves forging great programming solutions and exploring new
+              technologies. Welcome to my portfolio website.
             </p>
-            <img className="hero-pic" src="src/assets/me.png"></img>
           </div>
           <button className="cv-btn">My CV</button>
+          <a href="#about-me" className="arrow down"></a>
         </div>
-        <div className="main-content">
+        <div className="main-content" id="about-me">
           <div className="about-me-elements" ref={aboutMeRef}>
             <div className="typewriter">
-              <h2 className={aboutMeInView ? 'animate' : ""}>About Me.</h2>
+              <h2 className={aboutMeInView ? "animate" : ""}>About Me.</h2>
             </div>
-            <p className="about-me-text">
+            <img className="hero-pic" src="src/assets/me.png"></img>
+
+            <p style={{color: "white"}} className="about-me-text">
+              Hello there! I'm Sam Woodall, a Software Devloper based in West
+              Sussex, UK. For me, the joy in coding stems from the continual
+              problem solving required to create great code. Breaking down
+              problems, solving them piece by piece and then scaling them back
+              up to the point where you can step back to look at a functioning
+              tangible creation, I find, is immensily satisfying. The fact that
+              there is never one road to a solution with coding continually
+              makes the experience exciting and opens up endless oppurtunities
+              to learn and expand my skillset. I am commited to the endless learning
+              journey that comes with a career in Software Development and I am
+              eager to continue to learn and create fantastic programming solutions.
+            </p>
+
+            <p style={{color: "white"}} className="about-me-text">
+              Outside of coding, I like to keep up with my personal fitness
+              through weightlifting and running. I also love foreign language
+              learning and have been studying Korean as my focus, both formally and self study,
+              for around 7 years now.
+            </p>
+          </div>
+          <a href="#skillset" className="arrow down"></a>
+        </div>
+        <div className="main-content" id="skillset">
+          <div className="skillset-elements" ref={skillsetRef}>
+            <div className="typewriter">
+              <h2 className={skillsetInView ? "animate" : ""}>Skillset</h2>
+            </div>
+            <p className="skillset-text">
               I am a Software Engineer who loves forging great programming
               solutions and exploring new technologies. Welcome to my portfolio
               website.
+            </p>
+          </div>
+        </div>
+        <div className="main-content">
+          <div className="contact-elements" ref={contactRef}>
+            <div className="typewriter">
+              <h2 className={contactInView ? "animate" : ""}>Contact Me</h2>
+            </div>
+            <p className="contact-text">
+              Please use the below form to get in contact. Alternatively use the
+              LinkedIn Icon to contact me there.
             </p>
           </div>
         </div>
