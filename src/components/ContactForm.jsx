f88351f91dc7e407fd670a9e3ecdcf3c33cc231d
swoody2527 <swoodall2527@gmail.com>
2024-03-10 16:41:19 +0000

    contact form functionality

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,71 +1,116 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { useInView } from "react-intersection-observer";
-import emailjs from 'emailjs-com'
+import emailjs from "emailjs-com";
+import axios from "axios";
 
 function ContactForm() {
+  const [contactRef, contactInView] = useInView({
+    triggerOnce: true,
+  });
 
-    
-    const [contactRef, contactInView] = useInView({
-        triggerOnce: true,
-      });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        subject: "",
-        message: ""
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // const handleSubmit = (e) => {
+  //     e.preventDefault()
+
+  //     emailjs.send(
+  //         import.meta.env.VITE_APP_EMAIL_SERVICE_ID,
+  //         import.meta.env.VITE_APP_EMAIL_TEMPLATE_ID,
+  //         formData,
+  //         import.meta.env.VITE_APP_EMAIL_USER_ID
+  //     )
+  //     .then((response) => {
+  //         console.log("success", response)
+  //     })
+  //     .catch((error) => {
+  //         console.log("Error", error)
+  //     })
+  // }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const formDataForSubmission = new FormData();
+    formDataForSubmission.append("name", formData.name);
+    formDataForSubmission.append("email", formData.email);
+    formDataForSubmission.append("subject", formData.subject);
+    formDataForSubmission.append("message", formData.message);
+
+    fetch("https://script.google.com/macros/s/AKfycbxNt_HbNk8QObtvLkYK8_JmSVy1x1MhGz7mMVF2lREHkn-5YTpzKs2TEGzaTEVullzySA/exec", {
+      method: "POST",
+      body: formDataForSubmission
     })
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        emailjs.send(
-            import.meta.env.VITE_APP_EMAIL_SERVICE_ID,
-            import.meta.env.VITE_APP_EMAIL_TEMPLATE_ID,
-            formData,
-            
-                import.meta.env.VITE_APP_EMAIL_USER_ID
-            
-        )
-        .then((response) => {
-            console.log("success", response)
-        })
-        .catch((error) => {
-            console.log("Error", error)
-        })
-    }
+    .then(reponse => {
+      console.log(reponse, "success");
+    })
+  
+    // try {
+    //   const response = await axios.post(
+    //     "https://script.google.com/macros/s/AKfycbygP-5FREVT41SCywQTxIizAaL2cB-TWTPMhE72ST_G8z9Y3PED0f7OkllVlhe0ppsHwA/exec",
+    //     formDataForSubmission
+    //   );
+  
+    //   console.log("Success", response);
+    // } catch (error) {
+    //   console.error("Error occurred", error);
+    // }
+  };
 
   return (
     <div className="main-content" id="contact-me">
-          <div className="contact-elements" ref={contactRef}>
-            <div className="typewriter">
-              <h2 className={contactInView ? "animate" : ""}>Contact Me.</h2>
-            </div>
-            <p className="contact-text">
-              Please use the below form to get in contact. Alternatively use the
-              LinkedIn Icon to contact me there.
-            </p>
-            <form onSubmit={handleSubmit} className="contact-form">
-              <div className="sender-details">
-              <input value={formData.name} name='name' placeholder="Name" onChange={handleChange}></input>
-              <input value={formData.email} name='email' placeholder="Email" onChange={handleChange}></input>
-              </div>
-              <div className="message-details">
-              <input value={formData.subject} name='subject' placeholder="Subject" onChange={handleChange}></input>
-              <textarea value={formData.message} name='message' className="message-body" placeholder="Message" onChange={handleChange}></textarea>
-              </div>
-              <button className="cv-btn" type="submit">Send</button>
-            </form>
-          </div>
+      <div className="contact-elements" ref={contactRef}>
+        <div className="typewriter">
+          <h2 className={contactInView ? "animate" : ""}>Contact Me.</h2>
         </div>
-  )
+        <p className="contact-text">
+          Please use the below form to get in contact. Alternatively use the
+          LinkedIn Icon to contact me there.
+        </p>
+        <form onSubmit={handleSubmit} className="contact-form">
+          <div className="sender-details">
+            <input
+              value={formData.name}
+              name="name"
+              placeholder="Name"
+              onChange={handleChange}></input>
+            <input
+              value={formData.email}
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}></input>
+          </div>
+          <div className="message-details">
+            <input
+              value={formData.subject}
+              name="subject"
+              placeholder="Subject"
+              onChange={handleChange}></input>
+            <textarea
+              value={formData.message}
+              name="message"
+              className="message-body"
+              placeholder="Message"
+              onChange={handleChange}></textarea>
+          </div>
+          <button className="cv-btn" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
